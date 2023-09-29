d466dc08a05a94f80df3d19f75a31c3f52229c01
Astrid <github@astrid.exposed>
2023-09-29 16:56:09 +0200

    Wording

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import fetch from "node-fetch";
 import path from "node:path";
 
-const hosts = ["https://setup.rbxcdn.com", "https://setup-ak.rbxcdn.com", "https://s3.amazonaws.com/setup.roblox.com"];
+const mirrors = ["https://setup.rbxcdn.com", "https://setup-ak.rbxcdn.com", "https://s3.amazonaws.com/setup.roblox.com"];
 
 /**
  * @returns {Promise.<String>} path/to/executable
  */
 export default async function getPath() {
 	let version;
-	for (const host of hosts) {
-		const response = await fetch(host + "/version");
+	for (const mirror of mirrors) {
+		const response = await fetch(mirror + "/version");
 		if (response.ok) {
 			version = await response.text();
 			break;
 		}
 	}
 	if (!version) {
-		throw new Error("No provider available");
+		throw new Error("No mirror available");
 	}
 	return path.join(process.env.LOCALAPPDATA, "Roblox\\Versions", version, "RobloxPlayerBeta.exe");
 }
