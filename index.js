4e9d3e65b2db6dca5ea6c56caeb55264da83023c
Astrid <github@astrid.exposed>
2023-09-29 19:16:53 +0200

    Revert

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 import fetch from "node-fetch";
 import path from "node:path";
 
-const mirrors = ["https://setup.rbxcdn.com", "https://setup-ak.rbxcdn.com", "https://s3.amazonaws.com/setup.roblox.com"];
-
 /**
  * @returns {Promise.<String>} path/to/executable
  */
 export default async function getPath() {
-	let version;
-	for (const mirror of mirrors) {
-		const response = await fetch(mirror + "/version");
-		if (response.ok) {
-			version = await response.text();
-			break;
-		}
+	let clientSettings = await fetch("https://clientsettingscdn.roblox.com/v2/client-version/WindowsPlayer/channel/LIVE");
+	if (clientSettings.ok) {
+		const { clientVersionUpload } = await clientSettings.json();
+		return path.join(process.env.LOCALAPPDATA, "Roblox\\Versions", clientVersionUpload, "RobloxPlayerBeta.exe");
+	} else {
+		throw new Error("clientsettingscdn.roblox.com returned", clientSettings.status);
 	}
-	if (!version) {
-		throw new Error("No mirror available");
-	}
-	return path.join(process.env.LOCALAPPDATA, "Roblox\\Versions", version, "RobloxPlayerBeta.exe");
 }
