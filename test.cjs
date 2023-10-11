22370ce6a1537fafff7b1dcec0dc00218ae5e6d4
Astrid <github@astrid.exposed>
2023-10-11 14:47:43 +0200

    Update test

--- test.cjs
+++ test.cjs
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-import("./index.js").then((module) => module.default().then(console.log));
+import("./index.js").then(({ default: module }) => {
+	module().then((path) => {
+		console.log(path);
+	});
+});
